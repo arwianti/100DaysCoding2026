a85b8e687247214ead359efb5ea47e8db3ff474e
arwianti <arwiarwianti@gmail.com>
2026-09-02 22:36:17 +0800

    day1

---
+++ day1.java
@@ -0,0 +1,12 @@
+
+public class Main {
+  // Class: Wadah utama seluruh kode program Java
+
+    // Main Method: Pintu masuk utama agar program bisa dijalankan
+    public static void main(String[] args) {
+
+        // Output: Perintah untuk mencetak teks ke layar 
+        System.out.println("hello world");
+
+    }
+}
