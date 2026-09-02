9aa195b01b7a68732cd621402ce5f727004a17e8
arwianti <arwiarwianti@gmail.com>
2026-09-02 23:24:48 +0800

    day1

--- day1.java
+++ day1.java
@@ -1,7 +1,12 @@
 
 public class Main {
   // Class: Wadah utama seluruh kode program Java
-
+  
+/*komentar multi baris: diawali dan diakhiri dengan garis miring dan bintang. 
+      /*ini adalah komentar multi-baris
+      *dan seperti ini jika dienter
+      */
+  
     // Main Method: Pintu masuk utama agar program bisa dijalankan
     public static void main(String[] args) {
 
