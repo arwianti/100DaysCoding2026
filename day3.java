114e87fa40df855b1e83ba96e92e07d2e44f1e19
arwianti <arwiarwianti@gmail.com>
2026-09-04 18:48:39 +0800

    day.3

---
+++ day3.java
@@ -0,0 +1,16 @@
+public class TokoBuku {
+    public static void main(String[] args) {
+        System.out.println("==================================");
+        System.out.println("       TOKO BUKU: \"GARUDA\"");
+        System.out.println("==================================");
+        System.out.println("Promo hari ini: \"Beli 2 Gratis 1\"");
+        System.out.println();
+        System.out.println("Item    harga");
+        System.out.println("Novel   Rp 55.000");
+        System.out.println("Komik   Rp 25.000");
+        System.out.println("Atlas   Rp 80.000");
+        System.out.println();
+        System.out.println("FIle struk disimpan di: D:\\Toko\\Struk");
+        System.out.println("Terima kasih sudah belanja di Toko GARUDA");
+    }
+}
