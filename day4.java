5f1fb3ddc8626cec9981c783f1fb3b59ada7b687
arwianti <arwiarwianti@gmail.com>
2026-09-05 19:43:57 +0800

    day4

---
+++ day4.java
@@ -0,0 +1,13 @@
+public class Day4{
+    public static void main(String[] args) {
+        byte umur = 18;
+        short JumlahTabungan = 3000;
+        int jajan = 2000;
+        long kost = 50000;
+        System.out.println("Umur saya sekarang "+umur+" tahun");
+        System.out.println("Jumlah tabungan : "+JumlahTabungan+" uang");
+        System.out.println("jajan perhari: Rp "+jajan);
+        System.out.println("biaya kost perbulan : "+kost" bulan");        
+        
+    }
+}
