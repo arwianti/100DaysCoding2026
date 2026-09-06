3b9bf6d127d23480c332f6146afbc3f219d9a1fd
arwianti <arwiarwianti@gmail.com>
2026-09-06 20:09:25 +0800

    day5

---
+++ day5.java
@@ -0,0 +1,14 @@
+public class day5 {
+    public static void main(String[] args) {
+        float tinggiBadan = 161.3f;
+        float beratBadan = 55.5f;
+        System.out.println("Tinggi Badan saya : " + tinggiBadan);
+        System.out.println("Berat Badan saya : " + beratBadan);
+        
+        double pi = 788.78000009;
+        double angka = 321.6789;
+        System.out.println("Nilai Pi = " + pi);
+        System.out.println("Nilai Angka = " + angka);
+   
+    }  
+}
