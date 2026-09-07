7514207ed52ed7634047919997fc11ee7fa4c7d9
arwianti <arwiarwianti@gmail.com>
2026-09-07 21:09:13 +0800

    day6

---
+++ day6.java
@@ -0,0 +1,15 @@
+public class Day6{
+
+  
+    public static void main(String[] args) {
+        char inisialNama = 'A';
+        boolean javasusah = true;
+
+
+
+      
+        System.out.println("inisial nama saya : " + inisialNama);
+        System.out.println("Java itu susah : " + javasusah);
+      
+    }  
+  }
