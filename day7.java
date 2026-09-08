71f2793e273bd89799dbf16ccf0a1f1222e082fb
arwianti <arwiarwianti@gmail.com>
2026-09-08 18:26:38 +0800

    day7

---
+++ day7.java
@@ -0,0 +1,22 @@
+ day7.java;
+
+public class Day8Java {
+
+    
+    public static void main(String[] args) {
+       String SMAN1Aralle = " sekolah menengah atas di aralle kabupaten mamasa ";
+       String Kejuruan = " disekolah ini ada dua kejuruan yaitu IPA dan IPS ";
+       String KejuruanIPA = " di kejuruan ipa kita belajar banyak tentang berhitung ";
+       String berhitung = " yaitu untuk memahami konsep dasar matematika dan ";
+       
+       //string untuk menyimpan teks atau sekumpulan karakter yang ditulis dengan tanda petik ganda (")
+       
+       
+        System.out.println("SMA NEGERI 1 ARALLE " + SMAN1Aralle);
+        System.out.println("kejuruan" + Kejuruan);
+        System.out.println("kejuruan IPA " + KejuruanIPA);
+        System.out.println("belajar berhitung"+ berhitung);
+       
+    }
+    
+}
