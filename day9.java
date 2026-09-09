baa6c33310422f08e8577f299454974fb76f4977
arwianti <arwiarwianti@gmail.com>
2026-09-09 19:53:43 +0800

    day8

---
+++ day9.java
@@ -0,0 +1,35 @@
+public class Day8{
+ 
+    public static void main(String[] args) {
+      //deklarasi
+  String alamat;
+  int NomorRumah;
+
+
+  //inisialisasi
+
+
+  alamat = "Saluhussu";
+  NomorRumah = 12 ;
+
+    //nilai awal
+
+ System.out.println("alamat rumah saya : " + alamat) ;
+ System.out.println(" dengan nomor rumah : " + NomorRumah) ;
+
+
+
+
+  // update variabel
+
+  alamat = "kalabe" ;
+   NomorRumah = 15 ;
+
+
+  System.out.println("alamat rumah saya : " + alamat) ;
+  System.out.println("dengan nomor rumah : " + NomorRumah) ;
+
+
+    }
+
+}
