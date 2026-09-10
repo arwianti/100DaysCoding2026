01c3d5953a328cd854d44fbb327c199a1708c14e
arwianti <arwiarwianti@gmail.com>
2026-09-10 21:32:26 +0800

    Day.9
    Implement main method to demonstrate usage of final variable.

---
+++ Day9.java
@@ -0,0 +1,20 @@
+package day10;
+
+
+public class day9 {
+
+    
+    public static void main(String[] args) {
+      
+       //guanakan 'final' untuk membuat bilangan konstanta;
+        
+         final int HargaBaju = 50000;
+        System.out.println("harga awal baju : " + HargaBaju);
+          
+        //penjelasan mengenai final variable; 
+        //final variabel : variabel yang nilainya atau isinya tidak dapat diubah setelah di inisialisasi;
+        //jika nilai konstanta diubah maka akan terjadi eror;
+        
+        
+    }
+}
