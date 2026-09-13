4ed1f57b578f0df534fcd2ca2f506c3e263ab8ab
arwianti <arwiarwianti@gmail.com>
2026-09-13 20:11:04 +0800

    day12

---
+++ day12.java
@@ -0,0 +1,11 @@
+public class main{
+public static void main(String[] args) {
+
+  String a = "arwi.";
+  String b = " olahraga";
+
+
+  System.out.println("halo, nama saya " + a + (" hobi saya " + b)) ;
+
+}
+}
