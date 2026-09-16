de514784766e3ebd5571e47bc153433c77c8a45c
arwianti <arwiarwianti@gmail.com>
2026-09-16 23:23:33 +0800

    day15

---
+++ day15.java
@@ -0,0 +1,17 @@
+public class Day15.java{
+  public static void main(String[] args) {
+
+     int a = 20;
+     int b = 12;
+
+  //perkalian
+  int perkalian = a * b ;
+
+  // pembagian
+  int pembagian = a / b ;
+
+
+    System.out.println("hasil perkalian: " + perkalian + ( "\nhasil pembagian: " + pembagian )) ;
+    
+  }
+}
