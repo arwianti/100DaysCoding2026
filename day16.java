2b7b1897485acd77cc5ba52d07710fa4828798d8
arwianti <arwiarwianti@gmail.com>
2026-09-17 23:10:35 +0800

    day16

---
+++ day16.java
@@ -0,0 +1,11 @@
+public class Day16.java{
+  public static void main(String[] args) {
+
+  int a = 17, n=5;
+
+  int sisa = a%n;
+
+  System.out.println("sisa pembagian 17 oleh 5 adalah: " + sisa) ;
+
+  }
+}
