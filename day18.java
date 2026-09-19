95f14796b19623d8b1e2486a207be91816128b76
arwianti <arwiarwianti@gmail.com>
2026-09-19 21:02:11 +0800

    day18

---
+++ day18.java
@@ -0,0 +1,28 @@
+public class java {
+    public static void main(String[] args) {
+        byte b=11;
+        short s=b;
+        int i=s;
+        long l=i;
+        float f=l;
+        double d=f;
+        
+        
+        
+        System.out.println("dimulai dari bilangan ganjil:" );
+        
+        System.out.println("nilai byte:" + b);
+                
+        System.out.println("nilai short: " + b+s);
+        
+        System.out.println("nilai int: " + s+i );
+        
+        System.out.println("nilai long:" + i+l );
+        
+        System.out.println("nilai float:" + l+f);
+        
+        System.out.println("nilai double:" + f+d);
+        
+    }
+    
+}
