870485019ff2232f2ed6324900ef2543145aad41
arwianti <arwiarwianti@gmail.com>
2026-09-21 22:41:52 +0800

    day20

---
+++ day20.java
@@ -0,0 +1,11 @@
+package Day20;
+
+public class java {
+  public static void main(String[] args) {
+
+  Double angkaPrimitif=161.0;
+  String StrNilai= Double.toString(angkaPrimitif) ;
+
+  System out println("hasil konversi double menggunakan toString: " + StrNilai) ;
+  }
+}
