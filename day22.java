9a0c1e3185f5761b0c1384c9e1e87821140b0fb5
arwianti <arwiarwianti@gmail.com>
2026-09-23 21:35:52 +0800

    Add Java program to swap two integersday22

---
+++ day22.java
@@ -0,0 +1,18 @@
+package Day21;
+
+public class java{
+  public static void main (String[] args) {
+    int a=100;
+    int b=250;
+
+  System.out.println("sebelum ditukar \n a:"+a + "\n b:"+b) ;
+
+
+  a=a+b;
+  b=a-b;
+  a=a-b;
+
+  System.out.println(" setelah ditukar \n a:"+a + "\n b:"+b) ;
+
+  }
+}
